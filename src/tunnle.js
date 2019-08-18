16e3749ceec090cec8354ec2d37085ada0944b62
Immanuel Zumbruch <john.waterberg@outlook.com>
2019-08-18 23:28:57 +0200

    Definable Holes for the tunnle surface..

--- src/tunnle.js
+++ src/tunnle.js
@@ -12,16 +12,17 @@ var tunnlejs = (function () {
             }
         };
 
-
-        var outer = createWallByFunction(surfaceWallFunction(config.outerSurface,config.radius,config.height,config.resolution),config.resolution, config.outerSurface.length, -1)
-        var inner = createWallByFunction(surfaceWallFunction(config.innerSurface,config.radius*(1-config.thickness),config.height,config.resolution), config.resolution, config.innerSurface.length, 1)
+        var outer = createWallByFunction([{top:config.outerSurface.length-2,left:0,bottom:0,right:config.resolution-2}], surfaceWallFunction(config.outerSurface,config.radius,config.height,config.resolution),config.resolution, config.outerSurface.length, -1)
+        var inner = createWallByFunction([{top:config.outerSurface.length-2,left:0,bottom:0,right:config.resolution-2}], surfaceWallFunction(config.innerSurface,config.radius*(1-config.thickness),config.height,config.resolution), config.resolution, config.innerSurface.length, 1)
         return createTunnle(outer, inner);
     }
 
-    var createTunnleByWallFunctions = function (outerWallFunction, innerWallFunction, horizontalResolution, verticalResolution) {
+    var createTunnleByWallFunctions = function (holes, outerWallFunction, innerWallFunction, horizontalResolution, verticalResolution) {
         //Outer and inner wall of the tunnle
-        var outer = createWallByFunction(outerWallFunction,horizontalResolution, verticalResolution, -1)
-        var inner = createWallByFunction(innerWallFunction,horizontalResolution, verticalResolution, 1)
+        var outerHoles = JSON.parse(JSON.stringify(holes));
+        var innerHoles = JSON.parse(JSON.stringify(holes));
+        var outer = createWallByFunction(outerHoles,outerWallFunction,horizontalResolution, verticalResolution, -1)
+        var inner = createWallByFunction(innerHoles,innerWallFunction,horizontalResolution, verticalResolution, 1)
         return createTunnle(outer, inner);
     }
 
@@ -29,7 +30,7 @@ var tunnlejs = (function () {
 
     var createTunnle = function (outerSurface, innerSurface) {
             //Top and bottom closure (Will be extended in a future release)
-            var closure = createClosure(outerSurface.closureVertices, innerSurface.closureVertices);
+            var closure = createClosures(outerSurface.closureSets, innerSurface.closureSets);
     
             //Concat the triangles of the tunnle
             var obj = simplelinearalgebra.createObject(outerSurface.triangles.concat(innerSurface.triangles).concat(closure));
@@ -38,23 +39,30 @@ var tunnlejs = (function () {
             return simplestl.getFullTextSTL(obj);
     }
 
-    var createClosure = function (outerVertices, innerVertices) {
-        var triangles = [];        
-        for(var i = 0;i<outerVertices.length;i++) {
-            //Bottom closure
-            var v1 = outerVertices[i];
-            var v2 = outerVertices[(i+1) % (outerVertices.length)];
-            var v3 = innerVertices[i];
-            var v4 = innerVertices[(i+1) % (innerVertices.length)];
-            triangles.push(simplelinearalgebra.createTriangle(v1,v2,v4));
-            triangles.push(simplelinearalgebra.createTriangle(v3,v1,v4));
-
-
-        }
+    var createClosures = function (outerClosures, innerClosures) {
+        var triangles = [];     
+        for(var c = 0;c<outerClosures.length;c++){
+            var outerVertices = outerClosures[c];
+            var innerVertices = innerClosures[c];
+            for(var i = 0;i<outerVertices.length;i++) {
+                //Bottom closure
+                var v1 = outerVertices[i];
+                var v2 = outerVertices[(i+1) % (outerVertices.length)];
+                var v3 = innerVertices[i];
+                var v4 = innerVertices[(i+1) % (innerVertices.length)];
+                try{
+                triangles.push(simplelinearalgebra.createTriangle(v1,v2,v4));
+                triangles.push(simplelinearalgebra.createTriangle(v3,v1,v4));
+                }catch(e){
+                    var hhh = 1000;
+                }
+            }
+        }   
+        
         return triangles;
     }
-
-    var createWallByFunction = function (wallFunction, resolutionHorizontal, resolutionVertical, direction) {
+    //hole: {top:0,left:0,bottom:resolutionVertical-1,right:resolutionHorizontal-1}
+    var createWallByFunction = function (holes, wallFunction, resolutionHorizontal, resolutionVertical, direction) {
         var triangles = [];
         var closureVertices = [];
 
@@ -75,24 +83,68 @@ var tunnlejs = (function () {
                     }
                 }
 
-                if(i==0) closure.left.unshift(vertices[0]);
-                if(h==0) closure.bottom.push(vertices[2]);
-                if(i==resolutionHorizontal-2) closure.right.push(vertices[3]);
-                if(h==resolutionVertical-2) closure.top.unshift(vertices[1]);
+                    
+                fillHoles(holes,i,h,vertices);
 
-                
-                
-                if(direction < 0) {
-                    triangles.push(simplelinearalgebra.createTriangle(vertices[0],vertices[1],vertices[2]));
-                    triangles.push(simplelinearalgebra.createTriangle(vertices[1],vertices[3],vertices[2]));
-                } else {
-                    triangles.push(simplelinearalgebra.createTriangle(vertices[0],vertices[2],vertices[1]));
-                    triangles.push(simplelinearalgebra.createTriangle(vertices[1],vertices[2],vertices[3]));
+                if(!isInHole(holes,i,h)){
+                    if(direction < 0) {
+                        triangles.push(simplelinearalgebra.createTriangle(vertices[0],vertices[1],vertices[2]));
+                        triangles.push(simplelinearalgebra.createTriangle(vertices[1],vertices[3],vertices[2]));
+                    } else {
+                        triangles.push(simplelinearalgebra.createTriangle(vertices[0],vertices[2],vertices[1]));
+                        triangles.push(simplelinearalgebra.createTriangle(vertices[1],vertices[2],vertices[3]));
+                    }
                 }
             }
         }
-        closureVertices = closure.bottom.concat(closure.right).concat(closure.top).concat(closure.left);
-        return {triangles, closureVertices};        
+        
+        return {triangles, closureSets:createClosureSets(holes)};        
+    }
+
+    var isInHole = function (holes,x,y) {
+        for(var i = 1;i< holes.length;i++) {
+            var hole = holes[i];
+            if(x>=hole.left && x<=hole.right && y>=hole.bottom && y<=hole.top){
+                return true;
+            } 
+        }
+        return false;
+    }
+
+
+    var fillHoles = function (holes,x,y, vertices) {
+        for(var i = 0;i< holes.length;i++) {
+            if(!holes[i].leftVertices)holes[i].leftVertices = [];
+            if(!holes[i].rightVertices)holes[i].rightVertices = [];
+            if(!holes[i].topVertices)holes[i].topVertices = [];
+            if(!holes[i].bottomVertices)holes[i].bottomVertices = [];
+
+            if(i==0){
+                if(x>=holes[i].left && x<=holes[i].right && y>=holes[i].bottom && y<=holes[i].top){
+                    if(x==holes[i].left) holes[i].leftVertices.unshift(vertices[0]);
+                    if(y==holes[i].bottom) holes[i].bottomVertices.push(vertices[2]);
+                    if(x==holes[i].right) holes[i].rightVertices.push(vertices[3]);
+                    if(y==holes[i].top) holes[i].topVertices.unshift(vertices[1]);
+                }
+            } else {
+                if(x>=holes[i].left && x<=holes[i].right && y>=holes[i].bottom && y<=holes[i].top){
+                    if(x==holes[i].left) holes[i].leftVertices.push(vertices[1]);
+                    if(y==holes[i].bottom) holes[i].bottomVertices.unshift(vertices[0]);
+                    if(x==holes[i].right) holes[i].rightVertices.unshift(vertices[2]);
+                    if(y==holes[i].top) holes[i].topVertices.push(vertices[3]);
+                }
+            }
+        }
+    }
+
+    var createClosureSets = function (holes) {
+        var closureSets = [];
+        for(var i = 0;i< holes.length;i++) {
+            if(i==0)closureSets.push(holes[i].bottomVertices.concat(holes[i].rightVertices).concat(holes[i].topVertices).concat(holes[i].leftVertices));
+            if(i>0)closureSets.push(holes[i].leftVertices.concat(holes[i].topVertices).concat(holes[i].rightVertices).concat(holes[i].bottomVertices));
+            
+        }
+        return closureSets;
     }
 
 
